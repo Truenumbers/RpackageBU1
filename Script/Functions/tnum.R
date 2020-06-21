179eebba82b22225b70890e0d8534fb093189723
Allen Razdow <arazdow@truenum.com>
2020-06-21 18:27:14 -0400

    renamed global vars

--- Script/Functions/tnum.R
+++ Script/Functions/tnum.R
@@ -3,50 +3,99 @@
 require("httr")
 require("jsonlite")
 
-tnum.nspace <- ""
-tnum.nspaces <- ""
-tnum.token = ""
-tnum.ip = ""
+tnum.var.nspace <- ""
+tnum.var.nspaces <- ""
+tnum.var.token = ""
+tnum.var.ip = ""
 
-tnum.authorize <-function(ip="54.166.186.11"){
- 
+#' Title
+#'
+#' @param ip 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+tnum.authorize <- function(ip = "54.166.186.11") {
   assign("tnum.ip", ip, envir = .GlobalEnv)
-  result <- POST(paste0("http://",ip,"/v1/gateway/"),
-   body=paste0('{"email":"admin@truenumbers.com"}'),accept("application/json"),
-   content_type("application/json"))
-
+  result <- POST(
+    paste0("http://", ip, "/v1/gateway/"),
+    body = paste0('{"email":"admin@truenumbers.com"}'),
+    accept("application/json"),
+    content_type("application/json")
+  )
+  
   token <- content(result)$data$token
-  assign("tnum.token", token, envir = .GlobalEnv)
+  assign("tnum.var.token", token, envir = .GlobalEnv)
   ## get list of numberspaces
-  result <- GET(paste0("http://",ip,"/v1/numberspace/"),
-                 add_headers(Authorization = paste0("Bearer ", token)))
+  result <- GET(paste0("http://", ip, "/v1/numberspace/"),
+                add_headers(Authorization = paste0("Bearer ", token)))
   nspaces <- list()
-
-  for(x in content(result)$data){nspaces <- append(nspaces,x[[2]])}
-  assign("tnum.nspace", nspaces[[1]], envir = .GlobalEnv)
-  assign("tnum.nspaces", nspaces, envir = .GlobalEnv)
+  
+  for (x in content(result)$data) {
+    nspaces <- append(nspaces, x[[2]])
+  }
+  assign("tnum.var.nspace", nspaces[[1]], envir = .GlobalEnv)
+  assign("tnum.var.nspaces", nspaces, envir = .GlobalEnv)
   returnValue(nspaces)
-
+  
 }
 
-tnum.setspace <- function(name = "testspace"){
-  if(name %in% tnum.nspaces){
-  assign("tnum.nspace", name, envir = .GlobalEnv)
+#' Title
+#'
+#' @param name 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+tnum.setspace <- function(name = "testspace") {
+  if (name %in% tnum.nspaces) {
+    assign("tnum.var.nspace", name, envir = .GlobalEnv)
   } else {
     stop("server has no such numberspace")
   }
 }
 
-tnum.getspace <- function(){
-  returnValue(tnum.nspace)
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+tnum.getspace <- function() {
+  returnValue(tnum.var.nspace)
 }
 
 
-tnum.query <- function(query="* has *", max=10, start=0){
-  args <- list(numberspace = tnum.nspace, limit = max, offset = start, tnql = query)
+#' Title
+#'
+#' @param query 
+#' @param max 
+#' @param start 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+tnum.query <- function(query = "* has *",
+                       max = 10,
+                       start = 0) {
+  args <-
+    list(
+      numberspace = tnum.var.nspace,
+      limit = max,
+      offset = start,
+      tnql = query
+    )
   
-  result <- content(GET(paste0("http://",tnum.ip,"/v1/numberspace/numbers"), query=args,
-                add_headers(Authorization = paste0("Bearer ", tnum.token))))
+  result <-
+    content(GET(
+      paste0("http://", tnum.var.ip, "/v1/numberspace/numbers"),
+      query = args,
+      add_headers(Authorization = paste0("Bearer ", tnum.var.token))
+    ))
   message(paste0("Returned ", result$data$meta$records, " truenumbers"))
   returnValue(result$data$truenumbers)
+  
 }
